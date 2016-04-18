d16ff94d2e348a072a3a2c09dfcd9a1c38d8f20a
th1209 <hata.toshiki.1214@gmail.com>
2016-04-18 22:56:09 +0900

    main.js修正&背景をグラデーション表示

--- chap02/src/app.js
+++ chap02/src/app.js
@@ -13,6 +13,14 @@ var game = cc.Layer.extend({
     init:function(){
         this._super();
 
+        //背景画像にグラデーションをかける
+        //(第三引数にベクトルを渡せば、任意の方向でグラデーションをかけられる)
+        var gradient = cc.LayerGradient.create(
+            cc.color(0,0,0,255),            //黒。α値は255で最大であり、不透明となる
+            cc.color(0x64,0x82,0xB4,255));  //16進数でも記述可能
+        this.addChild(gradient);
+
+        //16個の背景を表示
         for(i = 0; i < 16; i++){
             var tile = cc.Sprite.create(res.cover_png);
             this.addChild(tile,0);
