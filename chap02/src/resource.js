807ae99b667befd5562d1e837fd2b72f798b74ba
th1209 <hata.toshiki.1214@gmail.com>
2016-04-18 22:12:06 +0900

    画像リソース追加

--- chap02/src/resource.js
+++ chap02/src/resource.js
@@ -1,5 +1,14 @@
+
 var res = {
-    HelloWorld_png : "res/HelloWorld.png",
+    cover_png : "res/cover.png",
+    tile_0png : "res/tile_0.png",
+    tile_1png : "res/tile_1.png",
+    tile_2png : "res/tile_2.png",
+    tile_3png : "res/tile_3.png",
+    tile_4png : "res/tile_4.png",
+    tile_5png : "res/tile_5.png",
+    tile_6png : "res/tile_6.png",
+    tile_7png : "res/tile_7.png"
 };
 
 var g_resources = [];
