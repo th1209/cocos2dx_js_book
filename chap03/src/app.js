e0d9fa7b33d0bc271cdebd0c542f1e76b0a3953d
th1209 <hata.toshiki.1214@gmail.com>
2016-04-19 23:36:25 +0900

    宇宙船を追加

--- chap03/src/app.js
+++ chap03/src/app.js
@@ -2,6 +2,8 @@
 var backGround;
 var gameLayer;
 var scrollSpeed = 1;
+var ship;
+var gameGravity = -0.05;
 
 
 //グローバルオブジェクト
@@ -17,14 +19,20 @@ var gameScene = cc.Scene.extend({
 var game = cc.Layer.extend({
     init:function(){
         this._super();
+
+        //LayerにSpriteを追加
         backGround = new ScrollingBG();
         this.addChild(backGround);
+        ship = new Ship();
+        this.addChild(ship);
+
         //scheduleUpdate関数は、フレームの更新毎に呼び出される
         this.scheduleUpdate();
     },
     //実際に呼ばれるのはupdate関数の処理
     update:function(dt){
         backGround.scroll();
+        ship.updateY();
     }
 });
 
@@ -44,6 +52,21 @@ var ScrollingBG = cc.Sprite.extend({
         if(this.getPosition().x < 0){
             this.setPosition(this.getPosition().x+480,this.getPosition().y);
         }
+    }
+});
+
+var Ship = cc.Sprite.extend({
+    ctor:function(){
+        this._super();
+        this.initWithFile(res.ship_png);
+        this.ySpeed = 0;
+    },
+    onEnter:function(){
+        this.setPosition(60,160);
+    },
+    updateY:function(){
+        this.setPosition(this.getPosition().x, this.getPosition().y+this.ySpeed);
+        this.ySpeed += gameGravity;
     }
 });
 
