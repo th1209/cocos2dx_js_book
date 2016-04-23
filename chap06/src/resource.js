9fdf80d42c09e3bbafd27800f56f42d09a9faee9
th1209 <hata.toshiki.1214@gmail.com>
2016-04-23 21:35:57 +0900

    プロジェクトスケルトン作成&画像リソース追加

---
+++ chap06/src/resource.js
@@ -0,0 +1,12 @@
+var res = {
+    bomb_png : "res/bomb.png",
+    cart_png : "res/cart.png",
+    strawberry_png : "res/strawberry.png",
+    leftbutton_png : "res/leftbutton.png",
+    rightbutton_png : "res/rightbutton.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
